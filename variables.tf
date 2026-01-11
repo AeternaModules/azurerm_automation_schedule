a258d732448a132d81e48e501cdcf94ac2aec6f1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:56:15 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "automation_schedules" {
+  description = <<EOT
+Map of automation_schedules, attributes below
+Required:
+    - automation_account_name
+    - frequency
+    - name
+    - resource_group_name
+Optional:
+    - description
+    - expiry_time
+    - interval
+    - month_days
+    - start_time
+    - timezone
+    - week_days
+    - monthly_occurrence (block):
+        - day (required)
+        - occurrence (required)
+EOT
+
+  type = map(object({
+    automation_account_name = string
+    frequency               = string
+    name                    = string
+    resource_group_name     = string
+    description             = optional(string)
+    expiry_time             = optional(string)
+    interval                = optional(number)
+    month_days              = optional(set(string))
+    start_time              = optional(string)
+    timezone                = optional(string, "Etc/UTC")
+    week_days               = optional(set(string))
+    monthly_occurrence = optional(object({
+      day        = string
+      occurrence = number
+    }))
+  }))
+}
+
